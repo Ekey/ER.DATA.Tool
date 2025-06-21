aa7b53d7eee86f7c9b4c224e7d4d33306eb0ea16
Christian Black <infinirt@hotmail.com>
2025-06-21 22:32:43 +0300

    Main

---
+++ ER.Metadata.Decryptor/ER.Metadata.Decryptor/FileSystem/Metadata/MetaHeader.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace ER.Metadata.Decryptor
+{
+    class Il2CppGlobalMetadataHeader
+    {
+        public Int32 magic { get; set; } // 0x504754 (TPG\0)
+        public Int32 sanity { get; set; }
+        public Int32 version { get; set; }
+        public Int32 stringLiteralOffset { get; set; }
+        public Int32 stringLiteralCount { get; set; }
+        public Int32 stringLiteralDataOffset { get; set; }
+        public Int32 stringLiteralDataCount { get; set; }
+        public Int32 stringOffset { get; set; }
+        public Int32 stringCount { get; set; }
+        public Int32 eventsOffset { get; set; }
+        public Int32 eventsCount { get; set; }
+        public Int32 propertiesOffset { get; set; }
+        public Int32 propertiesCount { get; set; }
+        public Int32 methodsOffset { get; set; }
+        public Int32 methodsCount { get; set; }
+        public Int32 parameterDefaultValuesOffset { get; set; }
+        public Int32 parameterDefaultValuesCount { get; set; }
+        public Int32 fieldDefaultValuesOffset { get; set; }
+        public Int32 fieldDefaultValuesCount { get; set; }
+        public Int32 fieldAndParameterDefaultValueDataOffset { get; set; }
+        public Int32 fieldAndParameterDefaultValueDataCount { get; set; }
+        public Int32 fieldMarshaledSizesOffset { get; set; }
+        public Int32 fieldMarshaledSizesCount { get; set; }
+        public Int32 parametersOffset { get; set; }
+        public Int32 parametersCount { get; set; }
+        public Int32 fieldsOffset { get; set; }
+        public Int32 fieldsCount { get; set; }
+        public Int32 genericParametersOffset { get; set; }
+        public Int32 genericParametersCount { get; set; }
+        public Int32 genericParameterConstraintsOffset { get; set; }
+        public Int32 genericParameterConstraintsCount { get; set; }
+        public Int32 genericContainersOffset { get; set; }
+        public Int32 genericContainersCount { get; set; }
+        public Int32 nestedTypesOffset { get; set; }
+        public Int32 nestedTypesCount { get; set; }
+        public Int32 interfacesOffset { get; set; }
+        public Int32 interfacesCount { get; set; }
+        public Int32 vtableMethodsOffset { get; set; }
+        public Int32 vtableMethodsCount { get; set; }
+        public Int32 interfaceOffsetsOffset { get; set; }
+        public Int32 interfaceOffsetsCount { get; set; }
+        public Int32 typeDefinitionsOffset { get; set; }
+        public Int32 typeDefinitionsCount { get; set; }
+        public Int32 rgctxEntriesOffset { get; set; }
+        public Int32 rgctxEntriesCount { get; set; }
+        public Int32 imagesOffset { get; set; }
+        public Int32 imagesCount { get; set; }
+        public Int32 assembliesOffset { get; set; }
+        public Int32 assembliesCount { get; set; }
+        public Int32 metadataUsageListsOffset { get; set; }
+        public Int32 metadataUsageListsCount { get; set; }
+        public Int32 metadataUsagePairsOffset { get; set; }
+        public Int32 metadataUsagePairsCount { get; set; }
+        public Int32 fieldRefsOffset { get; set; }
+        public Int32 fieldRefsCount { get; set; }
+        public Int32 referencedAssembliesOffset { get; set; }
+        public Int32 referencedAssembliesCount { get; set; }
+        public Int32 attributesInfoOffset { get; set; }
+        public Int32 attributesInfoCount { get; set; }
+        public Int32 attributeTypesOffset { get; set; }
+        public Int32 attributeTypesCount { get; set; }
+        public Int32 unresolvedVirtualCallParameterTypesOffset { get; set; }
+        public Int32 unresolvedVirtualCallParameterTypesCount { get; set; }
+        public Int32 unresolvedVirtualCallParameterRangesOffset { get; set; }
+        public Int32 unresolvedVirtualCallParameterRangesCount { get; set; }
+        public Int32 windowsRuntimeTypeNamesOffset { get; set; }
+        public Int32 windowsRuntimeTypeNamesSize { get; set; }
+        public Int32 exportedTypeDefinitionsOffset { get; set; }
+        public Int32 exportedTypeDefinitionsCount { get; set; }
+    }
+}
---
+++ ER.Metadata.Decryptor/ER.Metadata.Decryptor/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.IO;
+
+namespace ER.Metadata.Decryptor
+{
+    class Program
+    {
+        private static String m_MetaFile = "global-metadata.dat";
+        private static String m_MetaFileOut = "global-metadata-dec.dat";
+
+        static void Main(String[] args)
+        {
+            if (!File.Exists(m_MetaFile))
+            {
+                throw new FileNotFoundException("[ERROR]: Unable to open " + m_MetaFile + " file!");
+            }
+
+            var lpBuffer = File.ReadAllBytes(m_MetaFile);
+            using (MemoryStream TMetaStream = new MemoryStream(lpBuffer))
+            {
+                var m_Header = new Il2CppGlobalMetadataHeader();
+
+                m_Header.magic = TMetaStream.ReadInt32();
+
+                if (m_Header.magic != 0x504754)
+                {
+                    throw new Exception("[ERROR]: Invalid magic of metadata file!");
+                }
+
+                m_Header.sanity = TMetaStream.ReadInt32();
+                m_Header.version = TMetaStream.ReadInt32();
+                m_Header.stringLiteralOffset = TMetaStream.ReadInt32();
+                m_Header.stringLiteralCount = TMetaStream.ReadInt32();
+                m_Header.stringLiteralDataOffset = TMetaStream.ReadInt32();
+                m_Header.stringLiteralDataCount = TMetaStream.ReadInt32();
+                m_Header.stringOffset = TMetaStream.ReadInt32();
+                m_Header.stringCount = TMetaStream.ReadInt32();
+                m_Header.eventsOffset = TMetaStream.ReadInt32();
+                m_Header.eventsCount = TMetaStream.ReadInt32();
+                m_Header.propertiesOffset = TMetaStream.ReadInt32();
+                m_Header.propertiesCount = TMetaStream.ReadInt32();
+                m_Header.methodsOffset = TMetaStream.ReadInt32();
+                m_Header.methodsCount = TMetaStream.ReadInt32();
+                m_Header.parameterDefaultValuesOffset = TMetaStream.ReadInt32();
+                m_Header.parameterDefaultValuesCount = TMetaStream.ReadInt32();
+                m_Header.fieldDefaultValuesOffset = TMetaStream.ReadInt32();
+                m_Header.fieldDefaultValuesCount = TMetaStream.ReadInt32();
+                m_Header.fieldAndParameterDefaultValueDataOffset = TMetaStream.ReadInt32();
+                m_Header.fieldAndParameterDefaultValueDataCount = TMetaStream.ReadInt32();
+                m_Header.fieldMarshaledSizesOffset = TMetaStream.ReadInt32();
+                m_Header.fieldMarshaledSizesCount = TMetaStream.ReadInt32();
+                m_Header.parametersOffset = TMetaStream.ReadInt32();
+                m_Header.parametersCount = TMetaStream.ReadInt32();
+                m_Header.fieldsOffset = TMetaStream.ReadInt32();
+                m_Header.fieldsCount = TMetaStream.ReadInt32();
+                m_Header.genericParametersOffset = TMetaStream.ReadInt32();
+                m_Header.genericParametersCount = TMetaStream.ReadInt32();
+                m_Header.genericParameterConstraintsOffset = TMetaStream.ReadInt32();
+                m_Header.genericParameterConstraintsCount = TMetaStream.ReadInt32();
+                m_Header.genericContainersOffset = TMetaStream.ReadInt32();
+                m_Header.genericContainersCount = TMetaStream.ReadInt32();
+                m_Header.nestedTypesOffset = TMetaStream.ReadInt32();
+                m_Header.nestedTypesCount = TMetaStream.ReadInt32();
+                m_Header.interfacesOffset = TMetaStream.ReadInt32();
+                m_Header.interfacesCount = TMetaStream.ReadInt32();
+                m_Header.vtableMethodsOffset = TMetaStream.ReadInt32();
+                m_Header.vtableMethodsCount = TMetaStream.ReadInt32();
+                m_Header.interfaceOffsetsOffset = TMetaStream.ReadInt32();
+                m_Header.interfaceOffsetsCount = TMetaStream.ReadInt32();
+                m_Header.typeDefinitionsOffset = TMetaStream.ReadInt32();
+                m_Header.typeDefinitionsCount = TMetaStream.ReadInt32();
+                m_Header.rgctxEntriesOffset = TMetaStream.ReadInt32();
+                m_Header.rgctxEntriesCount = TMetaStream.ReadInt32();
+                m_Header.imagesOffset = TMetaStream.ReadInt32();
+                m_Header.imagesCount = TMetaStream.ReadInt32();
+                m_Header.assembliesOffset = TMetaStream.ReadInt32();
+                m_Header.assembliesCount = TMetaStream.ReadInt32();
+                m_Header.metadataUsageListsOffset = TMetaStream.ReadInt32();
+                m_Header.metadataUsageListsCount = TMetaStream.ReadInt32();
+                m_Header.metadataUsagePairsOffset = TMetaStream.ReadInt32();
+                m_Header.metadataUsagePairsCount = TMetaStream.ReadInt32();
+                m_Header.fieldRefsOffset = TMetaStream.ReadInt32();
+                m_Header.fieldRefsCount = TMetaStream.ReadInt32();
+                m_Header.referencedAssembliesOffset = TMetaStream.ReadInt32();
+                m_Header.referencedAssembliesCount = TMetaStream.ReadInt32();
+                m_Header.attributesInfoOffset = TMetaStream.ReadInt32();
+                m_Header.attributesInfoCount = TMetaStream.ReadInt32();
+                m_Header.attributeTypesOffset = TMetaStream.ReadInt32();
+                m_Header.attributeTypesCount = TMetaStream.ReadInt32();
+                m_Header.unresolvedVirtualCallParameterTypesOffset = TMetaStream.ReadInt32();
+                m_Header.unresolvedVirtualCallParameterTypesCount = TMetaStream.ReadInt32();
+                m_Header.unresolvedVirtualCallParameterRangesOffset = TMetaStream.ReadInt32();
+                m_Header.unresolvedVirtualCallParameterRangesCount = TMetaStream.ReadInt32();
+                m_Header.windowsRuntimeTypeNamesOffset = TMetaStream.ReadInt32();
+                m_Header.windowsRuntimeTypeNamesSize = TMetaStream.ReadInt32();
+                m_Header.exportedTypeDefinitionsOffset = TMetaStream.ReadInt32();
+                m_Header.exportedTypeDefinitionsCount = TMetaStream.ReadInt32();
+
+                TMetaStream.Seek(m_Header.stringOffset + 4, SeekOrigin.Begin);
+
+                var lpStrings = TMetaStream.ReadBytes(m_Header.stringCount);
+
+                UInt32 dwMagicCheck = BitConverter.ToUInt32(lpStrings, 0);
+
+                if (dwMagicCheck == 0x975ED124)
+                {
+                    lpStrings = Cipher.iDecryptData(lpStrings);
+
+                    TMetaStream.Seek(m_Header.stringOffset + 4, SeekOrigin.Begin);
+                    TMetaStream.Write(lpStrings, 0, lpStrings.Length);
+                    TMetaStream.Seek(4, SeekOrigin.Begin);
+
+                    lpBuffer = TMetaStream.ReadBytes((Int32)TMetaStream.Length - 4);
+                    File.WriteAllBytes(m_MetaFileOut, lpBuffer);
+                }
+
+                TMetaStream.Dispose();
+            }
+        }
+    }
+}
